ae8be03f1895e50096f04619f6537399e94688a3
Grena30 <grenadinu@yahoo.com>
2023-11-23 09:38:52 +0200

    feat: add basic menu

--- src/menu.asm
+++ src/menu.asm
@@ -1,7 +1,9 @@
-org 0x7c00  ; Origin of the bootloader program
+org 0x7c00			; Set the origin of the program to the bootloader's default address.
 
 section .data
-	option1 db "1. Key to Flp ", 0
+    menu db "Menu", 0
+    menu_len equ $ - menu
+    option1 db "1. Key to Flp ", 0
 	option1_len equ $ - option1
 	option2 db "2. Flp to RAM ", 0
 	option2_len equ $ - option2
@@ -11,64 +13,78 @@ section .data
 	input_len equ $ - input
 	wrong_input db "Invalid input", 0
 	wrong_input_len equ $ - wrong_input
-	row db 0
-	column db 0
-	
+
 section .text
 	global _start
 
 _start:
-	mov ah, 0  ; Video mode
-	mov al, 0x3  ; 80x25 text mode
-	int 10h 
-	
-print_menu:
-	mov si, option1
-	call print_option
-	mov si, option2
-	call print_option
-	mov si, option3
-	call print_option
-	mov si, input
-	call print_option
+	jmp print_menu
 
-print_option:
-	mov al, [si]     ; Load the character from the buffer
+print_menu:
+    mov bp, menu
+    call print_char
+    call insert_line
+
+	mov bp, option1
+	call print_char
+	call insert_line
+
+	mov bp, option2
+	call print_char
+	call insert_line
+
+	mov bp, option3
+	call print_char
+	call insert_line
+
+	mov bp, input
+	call print_char
+	call insert_line
+
+    ; TODO: Change hardcoded values
+	; Move the cursor to a specific position after printing the menu
+    mov ah, 02h     ; Set cursor position.
+    mov bh, 0       ; Set BH to 0 (video page).
+    mov dh, 4
+    mov dl, input_len       ; Set DL to the desired column.
+    int 10h         ; Set the new cursor position.
+
+print_char:
+	mov al, [bp]     ; Load the character from the buffer
 	cmp al, 0        ; Check if it's the null terminator
 	je reached_end  ; If it is, we reached the end
 
-	mov ah, 0x0e  ; Teletype output
+	mov ah, 0xe
 	int 10h
 
-	inc si  ; Next character in the buffer
-	jmp print_option
+	inc bp
+	jmp print_char
 
 reached_end:
-	ret  ; Return from the function
+	ret
 
-write_newline:
-	cmp dh, 24				; If at the last line of the terminal...
-	je scroll_down				; Scroll screen down 1 line
+insert_line:		; Label for inserting a new line.
+    cmp DH, 24		; Check if DH (row) is 24 (last row).
+    je scroll_down	; If so, jump to scroll_down.
 
-	;Query Cursor Position and Size
-	mov ah, 0x03				; DL, DH store cursor (x,y) positions
-	mov bh, 0
-	int 0x10
+    mov AH, 03h	    ; Get cursor position.
+    mov BH, 0		; Set BH to 0 (video page).
+    int 0x10		; Get the current cursor position.
+    jmp cursor_down	; Continue moving the cursor down.
 
-	jmp move_down	
+scroll_down:		; Label for scrolling down the screen.
+    mov AH, 0x06	; Set AH to 0x06 (scroll window up).
+    mov AL, 1		; Set AL to 1 (number of lines to scroll).
+    mov BH, 0x07	; Set BH to 0x07 (attribute for blank lines).
+    mov CX, 0		; Set CX to 0 (upper left corner).
+    mov DX, 0x184f	; Set DX to the bottom right corner.
+    int 0x10		; Scroll the window up.
+    mov DH, 0x17	; Set DH to 0x17 (last visible row).
 
-move_down:
-	mov ah, 0x02				; Move the cursor at the start of the line below this one
-	mov bh, 0
-	inc dh
-	mov dl, 0
-	int 0x10
-
-scroll_down:
-	mov ah, 0x06
-	mov al, 1
-	mov bh, 0x07				; Draw new line as White on Black
-	mov cx, 0					; (0,0): Top-left corner of the screen
-	mov dx, 0x184f				; (79,24): Bottom-right corner of the screen
-	int 0x10
-	mov dh, 0x17		
+cursor_down:		; Label for moving the cursor down by one line.
+    mov AH, 02h	    ; Set cursor position.
+    mov BH, 0		; Set BH to 0 (video page).
+    inc DH		; Increment DH to move down one row.
+    mov DL, 0		; Set DL to 0 (column).
+    int 0x10		; Set the new cursor position.
+    ret
