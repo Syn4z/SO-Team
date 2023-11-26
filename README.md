e9c757902281a2f9939757159d8d421a60882059
destiny <botnari_ciprian@mail.ru>
2023-11-26 13:54:43 +0200

    feat: calculate track, head, sector for each student

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # SO-Team #1
+
+## Task 1
+Given that a floppy disk is divided into 96 logical blocks with 30 sectors each, and each block has 15360 bytes, we can calculate the track, sector, and head for the given sector start and sector end.
+
+First, let’s calculate the total number of sectors per track. A standard 1.44MB floppy disk has 18 sectors per track and 2 heads (sides). So, the total number of sectors per track is 18 sectors/track * 2 heads = 36 sectors/track.
+
+Now, let’s calculate the track, sector, and head for the sector start (1951) and sector end (1980).
+
+For sector start (1951):
+
+    Track = 1951 / 36 = 54
+    Head = (1951 % 36) / 18 = 0
+    Sector = (1951 % 36) % 18 + 1 = 7
+
+For sector end (1980):
+
+    Track = 1980 / 36 = 55
+    Head = (1980 % 36) / 18 = 0
+    Sector = (1980 % 36) % 18 + 1 = 6
+
+So, to write to the floppy disk at sector start (1951), you would use track 54, head 0, and sector 7. And to write at sector end (1980), you would use track 55, head 0, and sector 6.
--- src/name.asm
+++ src/name.asm
@@ -1,40 +1,69 @@
-org 0x7c00
+org 1000h
 
-section .data
-    message_1: times 10 db "@@@FAF-213 Sorin IATCO###"
-    ; message_2: times 10 db "@@@FAF-213 Dinu GUTU###"
-    ; message_3: times 10 db "@@@FAF-213 Ciprian BOTNARI###"
-
-section .text
-    global _start
-
-_start:
-    ; Print string
-    mov ah, 0eh         ; Function code for print string
-    mov al, 0           ; Page number
-    mov bh, 0           ; Display page number
-    mov bl, 7           ; Text attribute
-    mov cx, 10          ; Number of characters to print
-    mov bp, message_1     ; Pointer to the string
-    int 10h             ; BIOS interrupt
-
+start:
     ; First sector
     mov ah, 03h        ; Function code for write sectors
     mov al, 1           ; Number of sectors to write
-    mov ch, 2           ; Cylinder number     
-    mov cl, 1           ; Sector number    
+    mov ch, 54           ; Cylinder number     
+    mov cl, 7           ; Sector number    
+    mov dl, 0
     mov dh, 1           ; Head number
-    mov bx, message_1   ; Pointer to the string
+    mov bx, student_1   ; Pointer to the string
     int 13h            ; BIOS interrupt
-
     ; Second sector
     mov ah, 03h        ; Function code for write sectors
     mov al, 1           ; Number of sectors to write
-    mov ch, 2           ; Cylinder number     
-    mov cl, 18           ; Sector number    
-    mov dh, 1           ; Head number
-    mov bx, message_1   ; Pointer to the string
+    mov ch, 55           ; Cylinder number     
+    mov cl, 6           ; Sector number    
+    mov dl, 0
+    mov dh, 0           ; Head number
+    mov bx, student_1   ; Pointer to the string
     int 13h            ; BIOS interrupt
 
-    ; mov ah, 4ch        ; Function code for program termination
-    ; int 21h             ; DOS interrupt
+    ; Third sector
+    mov ah, 03h        ; Function code for write sectors
+    mov al, 1           ; Number of sectors to write
+    mov ch, 65           ; Cylinder number     
+    mov cl, 5           ; Sector number    
+    mov dl, 0
+    mov dh, 1           ; Head number
+    mov bx, student_2   ; Pointer to the string
+    int 13h            ; BIOS interrupt
+    ; Fourth sector
+    mov ah, 03h        ; Function code for write sectors
+    mov al, 1           ; Number of sectors to write
+    mov ch, 65           ; Cylinder number     
+    mov cl, 6           ; Sector number    
+    mov dl, 0
+    mov dh, 2           ; Head number
+    mov bx, student_2   ; Pointer to the string
+    int 13h            ; BIOS interrupt
+
+    ; Fifth sector
+    mov ah, 03h        ; Function code for write sectors
+    mov al, 1           ; Number of sectors to write
+    mov ch, 66           ; Cylinder number     
+    mov cl, 17           ; Sector number    
+    mov dl, 0
+    mov dh, 1           ; Head number
+    mov bx, student_3   ; Pointer to the string
+    int 13h            ; BIOS interrupt
+    ; Sixth sector
+    mov ah, 03h        ; Function code for write sectors
+    mov al, 1           ; Number of sectors to write
+    mov ch, 67           ; Cylinder number     
+    mov cl, 6           ; Sector number    
+    mov dl, 0
+    mov dh, 0           ; Head number
+    mov bx, student_3   ; Pointer to the string
+    int 13h            ; BIOS interrupt
+
+    ; Print error code
+    mov al, '0'
+    add al, ah
+    mov ah, 0eh
+    int 10h
+
+student_1: times 10 db "@@@FAF-213 Ciprian BOTNARI###"
+student_2: times 10 db "@@@FAF-213 Dinu GUTU###"
+student_3: times 10 db "@@@FAF-213 Sorin IATCO###"
